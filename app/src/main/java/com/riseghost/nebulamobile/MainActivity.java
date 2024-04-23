41179b5acd69dfb92e8d76520e42bbeabad5aad9
RiseGhost <jomiguelsa2mt@gmail.com>
2024-04-23 03:04:08 +0100

    Update login layout

--- app/src/main/java/com/riseghost/nebulamobile/MainActivity.java
+++ app/src/main/java/com/riseghost/nebulamobile/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,16 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NebulaRequestSessionsCookie r = new NebulaRequestSessionsCookie("https://nebula-env.com",getApplicationContext());
-        try {
-            r.join();
-            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(r.getSharedPreferencesFilename(),MODE_PRIVATE);
-            String cookie = sharedPreferences.getString("cookie","");
-            NebulaLogin login = new NebulaLogin("https://nebula-env.com",cookie,"miguel@ubi.pt","theo");
-            Toast.makeText(getApplicationContext(),login.getResponseJSON(),Toast.LENGTH_LONG).show();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        EditText url = findViewById(R.id.url);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+        Button btn_login = findViewById(R.id.btn_login);
+        NebulaRequestSessionsCookie r = new NebulaRequestSessionsCookie(url.getText().toString(),getApplicationContext());
+        btn_login.setOnClickListener((event) -> {
+            try {
+                r.join();
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(r.getSharedPreferencesFilename(),MODE_PRIVATE);
+                String cookie = sharedPreferences.getString("cookie","");
+                NebulaLogin login = new NebulaLogin(url.getText().toString(),cookie,email.getText().toString(),password.getText().toString());
+                Toast.makeText(getApplicationContext(),login.getResponseJSON(),Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(getApplicationContext(),"Erro to connect server",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
